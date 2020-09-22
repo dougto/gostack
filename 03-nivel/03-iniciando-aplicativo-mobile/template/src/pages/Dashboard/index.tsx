99e57ea2b662bcdb39dfa93ce64de7c34e1ed510
Douglas Toledo da Silva <douglas22toledo@gmail.com>
2020-09-21 23:32:11 -0300

    started mobile dashboard

--- 03-nivel/03-iniciando-aplicativo-mobile/template/src/pages/Dashboard/index.tsx
+++ 03-nivel/03-iniciando-aplicativo-mobile/template/src/pages/Dashboard/index.tsx
@@ -1,25 +1,38 @@
 import React, {useCallback} from 'react';
-import {View, TouchableOpacity, Text} from 'react-native';
-import AsyncStorage from '@react-native-community/async-storage';
+import {} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+
+import {useAuth} from '../../hooks/auth';
+import {
+  Container,
+  Header,
+  HeaderTitle,
+  UserName,
+  ProfileButton,
+  UserAvatar,
+} from './styles';
 
 const Dashboard: React.FC = () => {
-  const signOut = useCallback(() => {
-    async function sign() {
-      await AsyncStorage.clear();
-      console.log('desloguei');
-    }
+  const {user} = useAuth();
+  const {navigate} = useNavigation();
 
-    sign();
-  }, []);
+  const navigateToProfile = useCallback(() => {
+    navigate('Profile');
+  }, [navigate]);
 
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <TouchableOpacity
-        onPress={signOut}
-        style={{width: 150, backgroundColor: '#fff'}}>
-        <Text style={{fontSize: 20}}>Deslogar</Text>
-      </TouchableOpacity>
-    </View>
+    <Container>
+      <Header>
+        <HeaderTitle>
+          Bem vindo, {'\n'}
+          <UserName>{user.name}</UserName>
+        </HeaderTitle>
+
+        <ProfileButton onPress={navigateToProfile}>
+          <UserAvatar source={{uri: user.avatar_url}} />
+        </ProfileButton>
+      </Header>
+    </Container>
   );
 };
 
